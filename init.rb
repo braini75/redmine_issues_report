41466c9ef46695a722a31ef72577c2b3675d59e5
Thomas K <braini@sonnenkinder.org>
2016-01-18 13:58:58 +0100

    3.x compatibility

--- init.rb
+++ init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_issues_report do
   description 'This Redmine plugin offers a detailed report of multiple issues.'
   version '0.1'
   url 'https://github.com/braini75/redmine_issues_report'
-  author_url 'https://github.com/braini75'
+  author_url 'https://github.com/braini75'  
   require 'issues_report_hooks'
 end
